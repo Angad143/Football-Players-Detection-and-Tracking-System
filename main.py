5873a070c081af32ca158f9cc5cd4d14ba7721ec
Angad143 <gangad419@rku.ac.in>
2024-09-03 17:32:58 +0530

    call TeamAssigner class from team_assigner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 import cv2
+from team_assigner import TeamAssigner
 def main():
     video_frames = read_video("inputs_videos/football_video_01.mp4")
 
@@ -19,6 +20,25 @@ def main():
     #     # save croppeed image of a players
     #     cv2.imwrite(f"output_videos/cropped_image.jpg", cropped_image)
     
+    # Initialize the TeamAssigner
+    team_assigner = TeamAssigner()
+
+    # Assign team colors to players based on their appearance in the first frame
+    team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+    
+    # Assign teams to players for each frame
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            # Determine the team for each player
+            team = team_assigner.get_player_team(video_frames[frame_num], 
+                                                 track['bbox'],
+                                                 player_id)
+            # Update the player's team and team color in the track
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+
+
     # Annotate video frames with object tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
